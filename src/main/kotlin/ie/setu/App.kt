511bad0ac1b66c19d92848bd1b5cc9a75128ba01
Dean Doyle <dean.doyle@ibm.com>
2025-12-19 12:54:34 +0000

    fix: automatic db setup

--- src/main/kotlin/ie/setu/App.kt
+++ src/main/kotlin/ie/setu/App.kt
@@ -2,9 +2,20 @@ package ie.setu
 
 import ie.setu.config.DbConfig
 import ie.setu.config.JavalinConfig
+import ie.setu.domain.db.Users
+import ie.setu.domain.db.Activities
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+
 
 fun main() {
 
     DbConfig().getDbConnection()
+    if (System.getenv("INIT_DB")?.lowercase() == "true") {
+        transaction {
+            SchemaUtils.createMissingTablesAndColumns(Users, Activities)
+        }
+    }
+q
     JavalinConfig().startJavalinService()
 }
